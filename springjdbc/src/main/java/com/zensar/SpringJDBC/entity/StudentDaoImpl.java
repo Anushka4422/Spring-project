653a15a173c837977de7db1a324544f2a7019bb0
ANUSHKA SINGH <105406115+Anushka4422@users.noreply.github.com>
2022-07-03 19:40:52 +0530

    Add files via upload

---
+++ springjdbc/src/main/java/com/zensar/SpringJDBC/entity/StudentDaoImpl.java
@@ -0,0 +1,42 @@
+package com.zensar.SpringJDBC.entity;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.zensar.SpringJDBC.dao.StudentDao;
+import com.zensar.SpringJDBC.entity.Student;
+
+public class StudentDaoImpl implements StudentDao {
+	private JdbcTemplate jdbcTemplate;
+
+	public int insert(Student student) {
+
+		// Insert query
+		String query = "insert into student(id,name,city) values(?,?,?)";
+		int r = this.jdbcTemplate.update(query, student.getId(), student.getName(), student.getCity());
+		return r;
+	}
+
+	public int change(Student student) {
+		// updating data
+		String query = "update student set name=? , city=? , where id=?";
+		int r = this.jdbcTemplate.update(query, student.getName(), student.getCity(), student.getId());
+		return r;
+	}
+
+	public int delete(int studentID) {
+		// delete operation
+		String query = "delete from student where id=?";
+		int r = this.jdbcTemplate.update(query, studentID);
+		return r;
+
+	}
+
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+}
